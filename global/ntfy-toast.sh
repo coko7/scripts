de2122f32cbc3229c416b4e7de0b705f29db1884
Coko <91132775+Coko7@users.noreply.github.com>
2026-06-04 11:25:45 +0200

    fix(ntfy-toast): use icons dir and fallback to missing texture icon

--- global/ntfy-toast.sh
+++ global/ntfy-toast.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-DEFAULT_ICON="$HOME/Pictures/System/navi.png"
+ICONS_DIR="$HOME/Pictures/icons/"
+DEFAULT_ICON="$ICONS_DIR/navi.png"
+MISSING_ICON="$ICONS_DIR/missing_texture.png"
 
 title="$1"
 body="$2"
@@ -13,15 +15,29 @@ if [ -z "$body" ]; then
   title="Hey listen!"
 fi
 
-if [ ! -f "$img_path" ]; then
-  img_path=$(fd "$img_path" "$HOME/Pictures/System")
-  if [ ! -f "$img_path" ]; then
-    img_path="$DEFAULT_ICON"
+# check if absolute path (starts with root)
+if [[ "$img_path" == /* ]]; then
+
+  # if exists
+  if [ -f "$img_path" ]; then
+    img_path_abs="$img_path"
+  else
+    img_path_abs="$MISSING_ICON"
+  fi
+else
+
+  # search in icons dir
+  search_result=$(fd "$img_path" "$ICONS_DIR")
+
+  if [ -f "$search_result" ]; then
+    img_path_abs="$search_result"
+  else
+    img_path_abs="$MISSING_ICON"
   fi
 fi
 
 notify-send --urgency=low "$title" "$body" \
-  --icon="$img_path" --expire-time=2000 \
+  --icon="$img_path_abs" --expire-time=2000 \
   --hint="string:x-canonical-private-synchronous:$hint" \
   --transient
 paplay "$sound_file"
